c42dcc2baa74218c8c8f10565baca038316a35dd
Jose Lopez <jose.lopezberro@gmail.com>
2025-01-16 13:34:58 +0100

    inicio form login y registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { RouterModule } from '@angular/router';
-import { LayoutPageComponent } from './auth/pages/layout-page/layout-page.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LayoutPageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,17 +3,21 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
 @NgModule({
   declarations: [
+    LayoutPageComponent,
     LoginPageComponent,
     RegisterPageComponent
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule
   ]
 })
 export class AuthModule { }
