c9079db0aa93a7115264bfbd84dc58cf23d64e9d
Jose Lopez <jose.lopezberro@gmail.com>
2025-01-27 12:57:41 +0100

    cambios de la autenticación y restricciones de acceso estando logueado  el usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 import { canActivateGuard, canMatchGuard } from './auth/guards/auth.guard';
+import { canActivateGuardAuth, canMatchGuardAuth } from './auth/guards/public.guard';
 
 // dominio.com/''
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    canActivate: [canActivateGuardAuth],
+    canMatch: [canMatchGuardAuth],
   },
   {
     path: 'heroes',
--- src/app/auth/guards/public.guard.ts
+++ src/app/auth/guards/public.guard.ts
@@ -1,8 +1,7 @@
-import { ActivatedRouteSnapshot, CanActivateFn, CanMatchFn, Route, Router, RouterStateSnapshot, UrlSegment } from "@angular/router";
-import { Observable, tap } from "rxjs";
-import { AuthService } from "../services/auth.service";
 import { inject } from "@angular/core";
-
+import { ActivatedRouteSnapshot, CanActivateFn, CanMatchFn, Route, Router, RouterStateSnapshot, UrlSegment } from "@angular/router";
+import { map, Observable, tap } from "rxjs";
+import { AuthService } from "../services/auth.service";
 
 
 
@@ -15,27 +14,28 @@ const checkAuthStatus = ():  Observable<boolean> => {
   return authService.checkAuthentication()
       .pipe(
         tap((isAuthenticated) => console.log('Authenticated: ', isAuthenticated)),
-        tap((isAuthenticated) => {
-          if (!isAuthenticated) {
-            router.navigate(['/auth/login'])
+        map((isAuthenticated) => {
+          if (isAuthenticated) {
+            router.navigate(['./']);
+                return false;
           }
+           return true;
         })
       )
   
 }
 
-
-export const canActivateGuard: CanActivateFn = ( //Hay que tener en cuenta el tipado CanActiveFn
+export const canActivateGuardAuth: CanActivateFn = ( //Hay que tener en cuenta el tipado CanActiveFn
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ) => {   
+
     return checkAuthStatus();
   };
 
-  export const canMatchGuard: CanMatchFn = ( //Tipado CanMatchFN
-    route: Route,
-    segments: UrlSegment[]
-  ) => {   
-    return checkAuthStatus();
-  };
-
+  export const canMatchGuardAuth: CanMatchFn = ( //Tipado CanMatchFN
+      route: Route,
+      segments: UrlSegment[]
+    ) => {   
+      return checkAuthStatus();
+    };
